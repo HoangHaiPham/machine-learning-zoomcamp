a8043da216d5b41941183d0f2976c9e5ec6a3cd9
Hoang Pham <h.hai.pham@cheil.com>
2024-12-20 11:51:55 +0100

    Done ML Zoomcamp 5.4 - Serving the Churn Model with Flask

--- 05-deployment/notebooks/predict.py
+++ 05-deployment/notebooks/predict.py
@@ -1,4 +1,8 @@
 import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
+
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -8,32 +12,23 @@ input_file = 'model_C=1.0.bin'
 with open(input_file, 'rb') as f_in: 
     dv, model = pickle.load(f_in)
 
+app = Flask('churn')
 
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
 
-X = dv.transform([customer])
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+    
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+    }
+    
+    return jsonify(result)
 
-y_pred = model.predict_proba(X)[0, 1]
-
-print('input', customer)
-print('churn probability', y_pred)
+# when run with gunicorn for deployment, this one will not be run.
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
